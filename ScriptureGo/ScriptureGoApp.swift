a59f731ac4fd177de9d93f8825eefae651662347
regalejandro <abc.regalejandro.03@gmail.com>
2025-11-17 16:35:15 -0800

    Initial Commit

---
+++ ScriptureGo/ScriptureGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScriptureGoApp.swift
+//  ScriptureGo
+//
+//  Created by Alejandro Regalado on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ScriptureGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

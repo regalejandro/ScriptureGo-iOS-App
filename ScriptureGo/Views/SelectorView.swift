f294a35d59691c38d60cd2fe4358273c2d9afc36
regalejandro <abc.regalejandro.03@gmail.com>
2025-11-21 16:46:52 -0800

    Added selector view and settings menu for translations

---
+++ ScriptureGo/Views/SelectorView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectorView.swift
+//  ScriptureGo
+//
+//  Created by Alejandro Regalado on 11/20/25.
+//
+
+import SwiftUI
+
+struct SelectorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectorView()
+}
---
+++ ScriptureGo/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  ScriptureGo
+//
+//  Created by Alejandro Regalado on 11/21/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
